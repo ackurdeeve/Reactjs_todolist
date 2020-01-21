748163718fb7ad0b35bf5e9383dad9a8390f795a
ackurdeeve <chenxuxuan@wust.edu.cn>
2020-01-22 00:07:45 +0800

    定义数据 绑定事件 通过setState修改状态修改数据

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,12 +1,24 @@
 import React, {Component, Fragment} from "react";
 
 class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        //定义数据
+        this.state = {
+            inputValue: 'Hello!!!',
+            list: []
+        }
+    }
+
     render() {
         return (
             //占位符
             <Fragment>
                 <div>
-                    <input/>
+                    <input
+                        //添加监听并绑定this，否则this = undefined
+                        onChange={this.handleInputChange.bind(this)}
+                        value={this.state.inputValue}/>
                     <button>提交</button>
                 </div>
                 <ul>
@@ -15,6 +27,15 @@ class TodoList extends Component {
                 </ul>
             </Fragment>
         );
+    }
+
+    handleInputChange(e){
+        // console.log(this);
+        // this.state.value = e.target.value;
+        this.setState({
+            inputValue: e.target.value
+        });
+        console.log(e.target.value)
     }
 }
 
