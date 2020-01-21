5fe4d81f9f68230332ee0df6fba2c7548e964394
ackurdeeve <chenxuxuan@wust.edu.cn>
2020-01-22 00:59:32 +0800

    绑定事件传递值给函数 ...另外一种写法，react核心概念，操作修改...复制的值，而不要直接修改state

--- src/TodoList.js
+++ src/TodoList.js
@@ -16,26 +16,57 @@ class TodoList extends Component {
             <Fragment>
                 <div>
                     <input
-                        //添加监听并绑定this，否则this = undefined
+                        //添加监听并绑定this与相应函数，否则this = undefined
                         onChange={this.handleInputChange.bind(this)}
-                        value={this.state.inputValue}/>
-                    <button>提交</button>
+                        value={this.state.inputValue}
+                    />
+                    <button onClick={this.handleBtnClick.bind(this)}>
+                        提交
+                    </button>
                 </div>
                 <ul>
-                    <li>学英语</li>
-                    <li>learning React</li>
+                    {
+                        this.state.list.map((item, index) => {
+                            return (
+                                //band可以添加传递的值给函数
+                                <li key={index}
+                                    onClick={this.handleItemDelete.bind(this, index)}>
+                                    {item}
+                                </li>
+                            )
+                        })
+                    }
                 </ul>
             </Fragment>
         );
     }
 
-    handleInputChange(e){
+    handleInputChange(e) {
         // console.log(this);
         // this.state.value = e.target.value;
         this.setState({
             inputValue: e.target.value
         });
         console.log(e.target.value)
+    }
+
+    handleBtnClick() {
+        this.setState({
+            //展开运算符
+            list: [...this.state.list, this.state.inputValue],
+            inputValue: ""
+        })
+    }
+
+    handleItemDelete(index) {
+        // console.log(index);
+        //immutable， state不允许直接改变，先...复制以后再改
+        const list = [...this.state.list];
+        list.splice(index, 1);
+
+        this.setState({
+            list: list
+        })
     }
 }
 
